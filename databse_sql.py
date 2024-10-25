ee454dcf0eb31db2f2bb2bad2f64bc55a372306f
dhanushya77 <dhanushyavs@gmail.com>
2024-10-25 16:15:56 +0530

    database

---
+++ databse_sql.py
@@ -0,0 +1,38 @@
+import sqlite3
+con=sqlite3.connect("batch2.db")
+try:
+    con.execute("create table user(No int, Name text, Age int)")
+except:
+    print("Table created")
+# con.execute("insert into user(No, Name, Age)values(1,'dhanushya',22),(2,'anu',20)")
+# con.commit()
+
+# no = int(input('Enter no:'))
+# name = input('Enter name:')
+# age = int(input('Enter age:'))
+# con.execute("insert into user(No, Name, Age)values(?, ?, ?)",(no, name, age))
+# con.commit()
+
+# limit = int(input('Enter limit:'))
+# for i in range(limit):
+#     no = int(input('Enter no:'))
+#     name = input('Enter name:')
+#     age = int(input('Enter age:'))
+#     con.execute("insert into user(No, Name, Age)values(?, ?, ?)",(no, name, age))
+#     con.commit()
+
+# data=con.execute("select * from user")
+# print(data)
+# print('{:<10}{:<20}{:<10}'.format('No','Name','Age'))
+# for i in data:
+#     print('{:<10}{:<20}{:<10}'.format(i[0],i[1],i[2]))
+
+# no=int(input('Enter no:'))
+# data=con.execute("select * from user where no=?",(no,))
+# for i in data:
+#     print(i)
+
+value=input('Enter value to be updated:')
+new_name=input('New name:')
+con.execute("update user set name=? where name=? ",(new_name,value,))
+con.commit()
--- mobile_no_validation.py
+++ mobile_no_validation.py
@@ -1,16 +1,19 @@
 import re
-# a=str(input('Enter mobile no:'))
-# if len(a)==10 and a.isdigit() and re.search('^[6-9].{9}',a):
-#     print('Valid mobile number')
-# else:
-#     print('Invalid mobile number')
+# mobile number validation
+a=str(input('Enter mobile no:'))
+if len(a)==10 and a.isdigit() and re.search('^[6-9].{9}',a):
+    print('Valid mobile number')
+else:
+    print('Invalid mobile number')
 
-# a=str(input('Enter email:'))
-# if re.search('^[a-z].{2}.*@gmail.com$',a):
-#     print('valid email')
-# else:
-#     print('invalid')
-
+# email validation
+a=str(input('Enter email:'))
+if re.search('^[a-z].{2}.*@gmail.com$',a):
+    print('valid email')
+else:
+    print('invalid')
+    
+# password validation
 a=str(input('enter password:'))
 if len(a)>=8 and re.search('[a-zA-Z].*[0-9].*',a):
     print('valid')
